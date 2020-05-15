2a29d69bccb845132b3bf1233b018b0dd1368f26
Guilherme Almeida <guigomesa@outlook.com>
2020-05-14 21:13:41 -0300

    FIX 2

--- TibiaApi.BotWeb/Pipelines/WorldListPipeline.cs
+++ TibiaApi.BotWeb/Pipelines/WorldListPipeline.cs
@@ -21,7 +21,6 @@ namespace TibiaApi.BotWeb.Pipelines
 
             foreach (var url in urls)
             {
-                Hangfire.Cron.HourInterval
                 Hangfire.RecurringJob.AddOrUpdate<WorldSpider>(url.Name, spider=> spider.Run(url.Url), Hangfire.Cron.Hourly(0), queue: Constantes.FilasConstantes.WORLD_SCRAPY);
             }
 
